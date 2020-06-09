59672696476dc4d4b5872205eadce652b2797663
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-06-09 19:25:00 +0600

    activity main

--- app/src/main/java/com/luminous/android/readwritetextfile/MainActivity.java
+++ app/src/main/java/com/luminous/android/readwritetextfile/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
         inputString = (EditText) findViewById(R.id.inputText);
         savedText = (TextView) findViewById(R.id.savedText);
-        
+        currentScore = (TextView) findViewById(R.id.scoreText);
+        saveButton = (Button) findViewById(R.id.saveButton);
     }
 }

d4602ff006271f805847971bc92308744ca9b96e
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-06-09 19:12:32 +0600

    activity main

--- app/src/main/java/com/luminous/android/readwritetextfile/MainActivity.java
+++ app/src/main/java/com/luminous/android/readwritetextfile/MainActivity.java
@@ -3,8 +3,19 @@ package com.luminous.android.readwritetextfile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText inputString;
+    TextView savedText;
+    TextView currentScore;
+    Button saveButton;
+    Button loadButton;
+    Button incrementButton;
+    Button decrementButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
